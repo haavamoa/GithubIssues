32a8bf7397c078b43ec460973270d55dbd85bd6b
Håvard Moås <hmo@dips.no>
2019-11-13 20:36:07 +0100

    Added repository for issue: https://github.com/xamarin/Xamarin.Forms/issues/5199

---
+++ src/FrameCornerRadius/FrameCornerRadius/Behaviours/RoundedFrameBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace FrameCornerRadius.Behaviours
+{
+    /// <summary>
+    /// A rounded frame behaviour, this will set the corner radius to the correct values if the frame has set its `HeightRequest`
+    /// Android : Corner radius should be the same as the height
+    /// iOS : Corner radius should be 58.33% of the height
+    /// </summary>
+    internal class RoundedFrameBehaviour : Behavior<Frame>
+    {
+        protected override void OnAttachedTo(BindableObject bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            if (!(bindable is Frame frame))
+                return;
+
+            frame.CornerRadius = Device.RuntimePlatform switch
+                {
+                Device.Android => (float)(frame.HeightRequest * 1),
+                Device.iOS => (float)(frame.HeightRequest * 0.5833),
+                _ => 0
+                };
+        }
+    }
+}
---
+++ src/FrameCornerRadius/FrameCornerRadius/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FrameCornerRadius
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private double m_frameCornerRadius;
+        private double m_frameHeightRequest;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public double FrameCornerRadius
+        {
+            get => m_frameCornerRadius;
+            set
+            {
+                m_frameCornerRadius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double FrameHeightRequest
+        {
+            get => m_frameHeightRequest;
+            set
+            {
+                m_frameHeightRequest = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand NavigateToWorkAroundCommand { get; } =
+            new Command(() => Application.Current.MainPage.Navigation.PushAsync(new WorkAroundPage()));
+    }
+}
