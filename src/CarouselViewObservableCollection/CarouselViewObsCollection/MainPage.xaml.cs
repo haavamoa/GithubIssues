ff635e7ef0619f8099ec2573fe95bbcb509b4de2
Håvard Moås <hmo@dips.no>
2020-06-02 13:06:44 +0200

    Added carouselview issue

---
+++ src/CarouselViewObservableCollection/CarouselViewObsCollection/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CarouselViewObservableCollection
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ViewModel
+    {
+
+        public ViewModel()
+        {
+            AddCommand = new Command(() =>
+            {
+                Items.Add($"Items {Items.Count}");
+                ((Command)RemoveLastCommand).ChangeCanExecute();
+            });
+            RemoveLastCommand = new Command(() =>
+            {
+                Items.Remove(Items.Last());
+                ((Command)RemoveLastCommand).ChangeCanExecute();
+            }, () => Items.Any());
+        }
+
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+
+        public ICommand AddCommand { get; }
+        public ICommand RemoveLastCommand { get; }
+    }
+}
--- src/ConverterParameter.Example/obj/Debug/netcoreapp3.0/MainWindow.g.i.cs
+++ src/ConverterParameter.Example/obj/Debug/netcoreapp3.0/MainWindow.g.i.cs
@@ -63,7 +63,7 @@ namespace ConverterParameter.Example {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ConverterParameter.Example;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ConverterParameter.Example;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
