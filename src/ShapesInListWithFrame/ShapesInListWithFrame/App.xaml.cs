d97721aafe2a3c210bd4e6fc542d33917325e275
Håvard Moås <hmo@dips.no>
2020-10-19 13:43:32 +0200

    Added ShapesInListWithFrame bug

---
+++ src/ShapesInListWithFrame/ShapesInListWithFrame/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShapesInListWithFrame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            Device.SetFlags(
+             new[]
+             {
+                    "Shapes_Experimental",
+             });
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
